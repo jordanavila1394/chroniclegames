50dbd0f459e6b7d17716d4fefa5f91010af18d2e
Avila George <george.avila@external.bper.it>
2024-03-23 12:04:23 +0100

    add app module header layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HomepageModule } from './pages/homepage/homepage.module';
+import { ContactsModule } from './pages/contacts/contacts.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomepageModule,
+    ContactsModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
